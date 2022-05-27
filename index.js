b4c0e9616ae683296344496a1e1782260e5ef4e1
roger10cassares <roger10cassares@gmail.com>
2022-05-27 17:59:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+// import App from './App';
+// import App from './src/screens/Home';
+import App from './src/examples/DraggableMarkers';
+
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
